c5e12d5bf21d89863ea10908609ae525d08ebab8
Vidhiyalakshmi C <cvidhiyalakshmi@gmail.com>
2026-07-30 14:36:04 +0530

    Add files via upload

---
+++ course.php
@@ -0,0 +1,11 @@
+<?php
+$name = $_GET["name"];
+$sslc = $_GET["sslcmrk"];
+$hsc = $_GET["hscmrk"];
+$course = $_GET["course"];
+
+echo "<b>NAME:</b> " . $name . "<br>";
+echo "<b>SSLC MARK:</b> " . $sslc . "<br>";
+echo "<b>HSC MARK:</b> " . $hsc . "<br>";
+echo "<b>COURSE SELECTED:</b> " . $course . "<br>";
+?>
